395ff8108ca8389bc21fb5a83462a0a6f9984197
PavelKotlov <pavelkotlov@gmail.com>
2023-03-09 03:17:59 -0800

    Create query to select all user reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -73,8 +73,29 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guest_id) {
+  const queryString = `
+  SELECT
+    reservations.id,
+    properties.*,
+    reservations.start_date,
+    AVG(property_reviews.rating)
+  FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+    WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date;`
+
+  return pool.query(queryString, [guest_id])
+  .then((result) => {
+    console.log(result.rows, result.rows.length);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return null;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
